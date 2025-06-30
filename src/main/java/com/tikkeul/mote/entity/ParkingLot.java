6c87dafe5f7dfb7c66f7dcf79f54b42afa8f10a9
Jaewoo Choi <jwoochoi1123@gmail.com>
2025-06-30 23:30:31 +0900

    fix: 관리자 회원가입 시 주차장 이름 항목 추가

--- src/main/java/com/tikkeul/mote/entity/ParkingLot.java
+++ src/main/java/com/tikkeul/mote/entity/ParkingLot.java
@@ -4,7 +4,9 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "parking_lot")
+@Table(name = "parking_lot", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "parking_lot_name")
+})
 @Getter
 @Setter
 @NoArgsConstructor
@@ -21,8 +23,11 @@ public class ParkingLot {
     @JoinColumn(name = "admin_id")
     private Admin admin;
 
+    @Column(name = "parking_lot_name", nullable = false, unique = true, length = 100)
+    private String parkingLotName;
+
     @Column(nullable = false, name = "base_price")
-    private int basePrice; // 기본 요금
+    private int basePrice;
 
     @Column(nullable = false, name = "price_per_minute")
     private Integer pricePerMinute;
