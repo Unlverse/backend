7d4d7617dad8011ac7665445d27997c923c891db
Jaewoo Choi <jwoochoi1123@gmail.com>
2025-07-01 15:56:34 +0900

    fix: 입차 요청 시 요청 보낸 시간 추가

--- src/main/java/com/tikkeul/mote/entity/EntryRequest.java
+++ src/main/java/com/tikkeul/mote/entity/EntryRequest.java
@@ -3,6 +3,8 @@ package com.tikkeul.mote.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "entry_request")
 @Getter
@@ -23,4 +25,12 @@ public class EntryRequest {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "admin_id", nullable = false)
     private Admin admin;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
