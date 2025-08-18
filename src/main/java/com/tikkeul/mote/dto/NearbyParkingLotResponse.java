fc945a5fd52de8a4bc64dc451889bfdce6429ffe
Jaewoo Choi <jwoochoi1123@gmail.com>
2025-08-18 17:03:08 +0900

    fix: 주변 주차장 조회 시 반환하는 정보 수정

--- src/main/java/com/tikkeul/mote/dto/NearbyParkingLotResponse.java
+++ src/main/java/com/tikkeul/mote/dto/NearbyParkingLotResponse.java
@@ -10,9 +10,8 @@ import lombok.*;
 public class NearbyParkingLotResponse {
     private Long adminId;        // parking_lot PK (= admin_id)
     private String name;         // parking_lot_name
+    private String address;
     private double latitude;     // parking_lot_latitude;
     private double longitude;    // parking_lot_longitude;
-    private int totalLot;
-    private int pricePerMinute;
     private double distanceKm;   // DB 계산값(km)
 }
